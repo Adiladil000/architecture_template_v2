281e933d57ece42c2b8e76a9b3489b9a0968b7b9
Adiladil0000 <hesenovadil757@gmail.com>
2023-12-07 01:52:47 +0400

    -  Color Schemes

--- lib/main.dart
+++ lib/main.dart
@@ -16,6 +16,7 @@ class _MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      theme: ThemeData.light(useMaterial3: true),
       localizationsDelegates: context.localizationDelegates,
       supportedLocales: context.supportedLocales,
       locale: context.locale,
