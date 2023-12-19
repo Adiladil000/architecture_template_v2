482d66d42e7274ee451e5d026132e25e5c059a23
Adiladil0000 <hesenovadil757@gmail.com>
2023-12-20 02:50:42 +0400

    -Added  3rd party packages

---
+++ module/common/lib/src/package/custom_image/custom_network_image.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+/// It will provide to image caching and image loading from network
+class CustomImageNetwork extends StatelessWidget {
+  const CustomImageNetwork({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
