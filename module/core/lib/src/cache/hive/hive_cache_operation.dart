d328e4180db0a6f80a3f0252469950eaa524763a
Adiladil0000 <hesenovadil757@gmail.com>
2024-01-13 16:31:54 +0400

    Cache Operations with Hive

---
+++ module/core/lib/src/cache/hive/hive_cache_operation.dart
@@ -0,0 +1,46 @@
+import 'package:core/src/cache/core/cache__operation.dart';
+import 'package:core/src/cache/core/cache_model.dart';
+import 'package:hive/hive.dart';
+
+/// The HiveCacheOperation class is an
+///  implementations of the CacheOperation class
+final class HiveCacheOperation<T extends CacheModel> extends CacheOperation<T> {
+  /// Initialize hive box
+  HiveCacheOperation() {
+    _box = Hive.box<T>(
+      name: T.toString(),
+    );
+  }
+
+  late final Box<T> _box;
+
+  @override
+  void add(T item) {
+    _box.put(item.id, item);
+  }
+
+  @override
+  void addAll(List<T> items) {
+    return _box.putAll(Map.fromIterable(items));
+  }
+
+  @override
+  void clear() {
+    return _box.clear();
+  }
+
+  @override
+  T? get(String id) {
+    return _box.get(id);
+  }
+
+  @override
+  List<T> getAll() {
+    return _box.getAll(_box.keys).where((element) => element != null).cast<T>().toList();
+  }
+
+  @override
+  void remove(String id) {
+    _box.delete(id);
+  }
+}
