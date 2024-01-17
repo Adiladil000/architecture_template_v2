63ddeac4caa4855b305ea412abf40d8e667f33ad
Adiladil0000 <hesenovadil757@gmail.com>
2024-01-17 15:30:01 +0400

    - Unit Test

---
+++ test/view_model/home_view_model_test.dart
@@ -0,0 +1,46 @@
+import 'package:architecture_template_v2/feature/home/view_model/home_view_model.dart';
+import 'package:architecture_template_v2/feature/home/view_model/state/home_state.dart';
+import 'package:bloc_test/bloc_test.dart';
+import 'package:flutter_test/flutter_test.dart';
+
+import 'mock/login_service_mock.dart';
+import 'mock/user_cache_mock.dart';
+
+void main() {
+  late final HomeViewModel homeViewModel;
+
+  setUp(() {
+    homeViewModel = HomeViewModel(
+      operationService: LoginServiceMock(),
+      userCacheOperation: UserCacheMock(),
+    );
+  });
+  group('Home View Model Test', () {
+    test('initial state is loading', () {
+      expect(homeViewModel.state.isLoading, false);
+    });
+
+    blocTest<HomeViewModel, HomeState>(
+      'change loading state',
+      build: () => homeViewModel,
+      act: (bloc) => bloc.changeLoading(),
+      expect: () => [
+        isA<HomeState>().having((state) => state.isLoading, 'isLoading', true),
+      ],
+    );
+
+    blocTest<HomeViewModel, HomeState>(
+      'fetch users',
+      build: () => homeViewModel,
+      act: (bloc) async => bloc.fetchUsers(),
+      expect: () => [
+        isA<HomeState>().having((state) => state.users, 'users', isNotEmpty),
+      ],
+    );
+
+    test('Check database for items', () async {
+      await homeViewModel.fetchUsers();
+      expect(homeViewModel.usersFromCache, isNotEmpty);
+    });
+  });
+}
